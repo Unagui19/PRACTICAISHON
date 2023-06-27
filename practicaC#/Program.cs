6a272ae55d9856da822961b4d4804ad227cfa218
Unagui19 <francorr199@gmail.com>
2023-06-27 16:18:11 -0300

    llenar arreglo con foreach sin metodo

--- practicaC#/Program.cs
+++ practicaC#/Program.cs
@@ -3,7 +3,7 @@
 int [] arreglo1=new int[3];
 int [] arreglo2={1,2,3};
 
-int arre;
+int arre, i=0;;
 //arreglo
 arreglo1[0]=1;
 arre=arreglo1[0];
@@ -11,15 +11,27 @@ Console.WriteLine(arre);
 
 Personal [] arreglo3 = new Personal[3];
 
-foreach (var pj in arreglo3)
-{
-    var personita =new Personal();
-    personita.Altura= new Random().Next(140,203);
-}
 
 
 //matrices
 int [,] matriz1 = new int[2,2];
 string [,] matriz2 = {{"a","b","c"},{"d","e","f"}};
 
+
+
+
+
+// FOREACH PARA COPIAS SIN METODO (UNA CACA)
+// foreach (var item in arreglo3)
+// {
+//     // int rand=new Random().Next(140,203);
+    
+//     var rand = new Random();
+//     var pers = new Personal();
+//     pers.Nombre=""+i;
+//     pers.Altura=rand.Next(140,205);
+//     Console.WriteLine(pers.Nombre);
+//     Console.WriteLine(pers.Altura);
+//     i++;
+// }
 
