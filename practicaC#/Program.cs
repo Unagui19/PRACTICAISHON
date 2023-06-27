ff1d3537336aef18126d922b0a919ace41ed6675
Unagui19 <francorr199@gmail.com>
2023-06-27 00:30:05 -0300

    rama de tipo de datos creada

---
+++ practicaC#/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// DateTime fecha= new DateTime(2023, 12, 19);
+// var fecha= new DateTime(2023, 12, 19);
+// var fActual = DateTime.Now;
+// Console.WriteLine(fecha);
+// Console.WriteLine(fActual);
+
+var dia= DateTime.Now.Day;
+Console.WriteLine(dia);
+var mes= DateTime.Now.Month;
+Console.WriteLine(mes);
+var anio= DateTime.Now.Year;
+Console.WriteLine(anio);
+var horaAct= DateTime.Now.TimeOfDay;
+Console.WriteLine(horaAct);
+
+
